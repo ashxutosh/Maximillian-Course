88e64eb4c047a93777da0ef04398a69bb445864d
ashxutosh <ashutoshrout002@gmail.com>
2026-05-14 17:53:33 +0530

    push kar le bhai

---
+++ react-practice/src/App.jsx
@@ -0,0 +1,8 @@
+import { Post } from './components/Post.jsx'
+
+function App() {
+
+  return (<Post/>)}
+   
+
+export default App
---
+++ react-practice/src/components/Post.jsx
@@ -0,0 +1,10 @@
+function Post(){
+    return(
+        <div>
+            <H1>Metal Post</H1>
+            <p>This is a simple blog post related metals</p>
+        </div>
+    )
+}
+
+export default Post
